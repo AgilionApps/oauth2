59e3f373c1732b92fd85c51bca1ed81b6e77f160
Alan Peabody <gapeabody@gmail.com>
2014-08-04 21:32:42 -0400

    Add dependencies.

--- mix.exs
+++ mix.exs
@@ -10,11 +10,14 @@ defmodule Oauth2.Mixfile do
 
   # Type `mix help compile.app` for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:logger, :postgrex, :ecto, :cowboy, :plug]]
   end
 
   # Type `mix help deps` for more examples and options
   defp deps do
-    []
+    [{:cowboy,   "~> 1.0.0"},
+     {:plug,     "~> 0.5.3"},
+     {:postgrex, ">= 0.0.0"},
+     {:ecto,     "~> 0.2.0"}]
   end
 end
